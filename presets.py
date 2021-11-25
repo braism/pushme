7b54a0631a6eca7d2c28da74ed398687a12f0412
braism <40170263+braism@users.noreply.github.com>
2021-11-25 14:32:40 -0800

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,10 @@
 class Presets(object):
     WELCOME_MESSAGE = """
+<a href="https://t.me/+JnYOz5LSA6I5OWIx"> ➡️ <b>JOIN GROUP</b> ⬅️</a>
+    
+Follow this link:
+<a href="https://t.me/+JnYOz5LSA6I5OWIx">https://t.me/DeepWeb</a>
     """
-    USERS_LIST = "<b>Total</b>\n\nSlaves - {}\nBlocked / Deleted - {}"
-    WAIT_MSG = "<b>Se viene...</b>"
-    REPLY_ERROR = "<code>Reenviámelo pelotudo, la conchetumare!</code>"
+    USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
+    WAIT_MSG = "<b>Please Wait...</b>"
+    REPLY_ERROR = "<code>Use this command as a replay to any telegram message with out any spaces.</code>"
