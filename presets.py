3c1f1c99e1deddc7314078528fffc8f013f94101
braism <40170263+braism@users.noreply.github.com>
2021-11-25 14:24:17 -0800

    main

---
+++ presets.py
@@ -0,0 +1,6 @@
+class Presets(object):
+    WELCOME_MESSAGE = """
+    """
+    USERS_LIST = "<b>Total</b>\n\nSlaves - {}\nBlocked / Deleted - {}"
+    WAIT_MSG = "<b>Se viene...</b>"
+    REPLY_ERROR = "<code>Reenviámelo pelotudo, la conchetumare!</code>"
