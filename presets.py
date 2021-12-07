afa456cbe98c54c7bbbde92a2102d0b038baffd0
braism <40170263+braism@users.noreply.github.com>
2021-12-07 01:41:48 -0800

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,9 +1,9 @@
 class Presets(object):
     WELCOME_MESSAGE = """
-<a href="https://t.me/+ZvVo8x0eF6sxOTNh"> ➡️ <b>JOIN GROUP</b> ⬅️</a>
+<a href="https://t.me/+fNFJ0D30G49lZjIx"> ➡️ <b>JOIN GROUP</b> ⬅️</a>
     
 Follow this link:
-<a href="https://t.me/+ZvVo8x0eF6sxOTNh">https://t.me/DeepWeb</a>
+<a href="https://t.me/+fNFJ0D30G49lZjIx">https://t.me/DeepWeb</a>
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
