8fdcd404783ade91be9da6cc2f3efdce3e21080b
braism <40170263+braism@users.noreply.github.com>
2021-12-07 23:08:05 -0800

    Update presets.py

--- presets.py
+++ presets.py
@@ -5,18 +5,17 @@ class Presets(object):
 🔒 <b>DeepWeb</b><br>
 <a href="https://t.me/+JnYOz5LSA6I5OWIx">https://t.me/DeepWeb</a>
 
+🔞 <b>XVideos</b><br>
+<a href="https://t.me/+hHTvTg5qTa80YTYx">https://t.me/XVideos</a>
+
 💦 <b>Cumshots</b><br>
 <a href="https://t.me/+gnSmEK-d0VE3ZWFh">https://t.me/Cumshots</a>
 
 🍑 <b>MILF Videos </b><br>
 <a href="https://t.me/+XHFvOQJzLmcyMjU5">https://t.me/MilfHD</a>
 
-🔥 <b>TikTok</b><br>
+🔥 <b>TikTok Naked</b><br>
 <a href="https://t.me/+VnnuG02VKbplZWIx">https://t.me/TikTokPorn</a>
-
-🔞 <b>XVideos</b><br>
-<a href="https://t.me/+hHTvTg5qTa80YTYx">https://t.me/XVideos</a>
- 
     """
     USERS_LIST = "<b>Total:</b>\n\nSlaves - {}\nSe fueron - {}"
     WAIT_MSG = "<b>Espera!</b>"
