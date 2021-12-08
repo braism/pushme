05fec7f0f00ac2aad28037b9a330ce3441d07f66
braism <40170263+braism@users.noreply.github.com>
2021-12-07 22:33:18 -0800

    Update presets.py

--- presets.py
+++ presets.py
@@ -5,7 +5,7 @@ class Presets(object):
 Follow this link:
 <a href="https://t.me/+gnSmEK-d0VE3ZWFh">https://t.me/DeepWeb</a>
     """
-    """
+    WELCOME_MESSAGE = """
 Please, don't share the content!
     """
     USERS_LIST = "<b>Total:</b>\n\nSlaves - {}\nSe fueron - {}"
