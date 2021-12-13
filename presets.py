c671d76e2f18479fb5aa55e2467c01b3a46cb9a7
braism <40170263+braism@users.noreply.github.com>
2021-12-13 00:49:14 -0800

    Update presets.py

--- presets.py
+++ presets.py
@@ -8,8 +8,11 @@ class Presets(object):
 🔞 <b>PORN KING</b><br>
 <a href="https://t.me/+gnSmEK-d0VE3ZWFh">https://t.me/XVideos</a>
 
-👅 <b>Oral & Blowjob </b><br>
-<a href="https://t.me/+1Sj-bWCGfsJmMmMx">https://t.me/OralBlows</a>
+🔥 <b>TikTok Naked</b><br>
+<a href="https://t.me/+VnnuG02VKbplZWIx">https://t.me/TikTokPorn</a>
+
+👅 <b>ONYLFANS </b><br>
+<a href="https://t.me/+aXR0waCssHAxYmZk">https://t.me/OnlyFanX</a>
 
 🍑 <b>MILF Videos </b><br>
 <a href="https://t.me/+XHFvOQJzLmcyMjU5">https://t.me/MilfHD</a>
@@ -19,9 +22,6 @@ class Presets(object):
 
 💦 <b>Cumshots</b><br>
 <a href="https://t.me/+hHTvTg5qTa80YTYx">https://t.me/Cumshots</a>
-
-🔥 <b>TikTok Naked</b><br>
-<a href="https://t.me/+VnnuG02VKbplZWIx">https://t.me/TikTokPorn</a>
     """
     USERS_LIST = "<b>Total:</b>\n\nSlaves - {}\nSe fueron - {}"
     WAIT_MSG = "<b>Espera!</b>"
