2904072e60e42374eb2eed717fcdb287409fd926
braism <40170263+braism@users.noreply.github.com>
2021-12-27 12:51:38 +0100

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,29 +3,30 @@ class Presets(object):
 <b>👇🏼 TOP +18 GROUPS 👇🏼</b>
 <br><br>
 🔒 <b>DeepWeb</b><br>
-<a href="https://t.me/+oq71_gBfebhjZTAx">https://t.me/DeepWeb</a>
+<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/DeepWeb</a>
 
 🔥 <b>XVideos</b><br>
-<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/XVideos</a>
+<a href="https://t.me/+uyoLxErod_5lZWZhh">https://t.me/XVideos</a>
 
 👅 <b>PornHub </b><br>
-<a href="https://t.me/+bhhyuq9AdQFhOGEx">https://t.me/PornHub</a>
+<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/PornHub</a>
 
 🍑 <b>MILF Videos </b><br>
-<a href="https://t.me/+XHFvOQJzLmcyMjU5">https://t.me/MilfHD</a>
+<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/MilfHD</a>
 
 💦 <b>OnlyFans</b><br>
-<a href="https://t.me/+U5u7NkJ3cAAwYTk0">https://t.me/OnlyFansX</a>
+<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/OnlyFansX</a>
 
 🔞 <b>Brazzers</b><br>
-<a href="https://t.me/+Hq_je5jIoYk5NDI5">https://t.me/Premium</a>
+<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/Premium</a>
 
 🍆 <b>Oral & Blowjob</b><br>
 <a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/OralXXX</a>
 
 🔞 <b>TikTok Naked</b><br>
-<a href="https://t.me/+wMG2JFExpTRmOTJh">https://t.me/TikTokX</a>
+<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/TikTokX</a>
     """
     USERS_LIST = "<b>Total:</b>\n\nSlaves - {}\nSe fueron - {}"
     WAIT_MSG = "<b>Espera!</b>"
     REPLY_ERROR = "<code>Así no weey!</code>"
+ 
