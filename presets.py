c033a623cb1fbbe026b46657631b8720eccc878d
Brais Workspace <me@brais.cloud>
2022-01-11 18:52:52 +0100

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,27 +3,28 @@ class Presets(object):
 <b>👇🏼 TOP +18 GROUPS 👇🏼</b>
 <br><br>
 🔒 <b>DeepWeb</b><br>
-<a href="https://t.me/+yXGK0qAAzKc3YjNh">https://t.me/DeepWeb</a>
+<a href="https://t.me/+eFCcsUUi8U0zNDYx">https://t.me/DeepWeb</a>
 
-👅 <b>PornHub </b><br>
-<a href="https://t.me/+bhhyuq9AdQFhOGEx">https://t.me/PornHub</a>
+👅 <b>TEENS </b><br>
+<a href="https://t.me/+PJh4_kw13wczMGYx">https://t.me/PornHub</a>
 
 🍑 <b>MILF Videos </b><br>
-<a href="https://t.me/+XHFvOQJzLmcyMjU5">https://t.me/MilfHD</a>
+<a href="https://t.me/+jSNgPcHKtS4yZWY5">https://t.me/MilfHD</a>
 
 💦 <b>OnlyFans</b><br>
-<a href="https://t.me/+uyoLxErod_5lZWZh">https://t.me/OnlyFansX</a>
+<a href="https://t.me/+lsRfu8N9qQczMzIx">https://t.me/OnlyFansX</a>
 
 🔞 <b>Brazzers</b><br>
 <a href="https://t.me/+NWBvfS9JhFs2NmE0">https://t.me/Premium</a>
 
 🍆 <b>Oral & Blowjob</b><br>
-<a href="https://t.me/+JnYOz5LSA6I5OWIx">https://t.me/OralXXX</a>
+<a href="https://t.me/+PJh4_kw13wczMGYx">https://t.me/OralXXX</a>
 
 🔞 <b>TikTok Naked</b><br>
-<a href="https://t.me/+VnnuG02VKbplZWIx">https://t.me/TikTokX</a>
+<a href="https://t.me/+k_h1bQuDRP81YTkx">https://t.me/TikTokX</a>
     """
     USERS_LIST = "<b>Total:</b>\n\nSlaves - {}\nSe fueron - {}"
     WAIT_MSG = "<b>Espera!</b>"
     REPLY_ERROR = "<code>Así no weey!</code>"
+ 
  
