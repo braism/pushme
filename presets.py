e9388600bc5a2bea95646b7e5fd23c9644f2f017
Brais Workspace <40170263+braism@users.noreply.github.com>
2022-04-12 02:58:00 +0200

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,13 +3,13 @@ class Presets(object):
 <b>👇🏼 TOP GROUPS 👇🏼</b>
 <br><br>
 🔒 <b>DeepWeb</b><br>
-<a href="https://t.me/+eFCcsUUi8U0zNDYx">https://t.me/DeepWeb</a>
+<a href="https://t.me/+N77He7hy5AU2MDBh">https://t.me/DeepWeb</a>
 
 🍓 <b>TEENS </b><br>
-<a href="https://t.me/+PJh4_kw13wczMGYx">https://t.me/Teens</a>
+<a href="https://t.me/+_yP5bb2n_p5iNjRh">https://t.me/Teens</a>
 
 🍑 <b>MILF Videos </b><br>
-<a href="https://t.me/+jSNgPcHKtS4yZWY5">https://t.me/MilfHD</a>
+<a href="https://t.me/+fgKN4TByK9VkZDkx">https://t.me/MilfHD</a>
 
 💦 <b>OnlyFans</b><br>
 <a href="https://t.me/+lsRfu8N9qQczMzIx">https://t.me/OnlyFans</a>
@@ -18,10 +18,10 @@ class Presets(object):
 <a href="https://t.me/+SzUUm_K2HIo0NTgx">https://t.me/Premium</a>
 
 🍆 <b>Oral & Blows</b><br>
-<a href="https://t.me/+PJh4_kw13wczMGYx">https://t.me/OralJob</a>
+<a href="https://t.me/+t1wnGQc6_u4zYzRh">https://t.me/OralJob</a>
 
 🔞 <b>TikTok</b><br>
-<a href="https://t.me/+k_h1bQuDRP81YTkx">https://t.me/TikTok18</a>
+<a href="https://t.me/+-l8Cl3eAASdhZDcx">https://t.me/TikTok18</a>
     """
     USERS_LIST = "<b>Total:</b>\n\nSlaves - {}\nSe fueron - {}"
     WAIT_MSG = "<b>Espera!</b>"
