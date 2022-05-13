0e8f0a26dfc1858c78679edf842ed0cbc7352e0e
Brais Workspace <40170263+braism@users.noreply.github.com>
2022-05-13 10:50:51 +0200

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,28 +3,16 @@ class Presets(object):
 <b>👇🏼 TOP GROUPS 👇🏼</b>
 <br><br>
 🔒 <b>DeepWeb</b><br>
-<a href="https://t.me/+YYph4Ta92u1mN2E5">https://t.me/DeepWeb</a>
+<a href="https://t.me/+utommBWTOlgwMWQx">https://t.me/DeepWeb</a>
 
 🍓 <b>TEENS </b><br>
-<a href="https://t.me/+YYph4Ta92u1mN2E5">https://t.me/Teens</a>
-
-🍑 <b>MILF Videos </b><br>
-<a href="https://t.me/+YYph4Ta92u1mN2E5">https://t.me/MilfHD</a>
-
-💦 <b>OnlyFans</b><br>
-<a href="https://t.me/+YYph4Ta92u1mN2E5">https://t.me/OnlyFans</a>
-
-🌭 <b>Hotdog</b><br>
-<a href="https://t.me/+YYph4Ta92u1mN2E5">https://t.me/Premium</a>
-
-🍆 <b>Oral & Blows</b><br>
-<a href="https://t.me/+YYph4Ta92u1mN2E5">https://t.me/OralJob</a>
+<a href="https://t.me/+utommBWTOlgwMWQx">https://t.me/Teens</a>
 
 🔞 <b>TikTok</b><br>
-<a href="https://t.me/+YYph4Ta92u1mN2E5">https://t.me/TikTok18</a>
+<a href="https://t.me/+utommBWTOlgwMWQx">https://t.me/TikTok18</a>
     """
     USERS_LIST = "<b>Total:</b>\n\nSlaves - {}\nSe fueron - {}"
     WAIT_MSG = "<b>Espera!</b>"
-    REPLY_ERROR = "<code>Así no weey!</code>"
+    REPLY_ERROR = "<code>Vuelve a intentarlo jeje</code>"
  
  
